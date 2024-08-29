5bb57e0dc3bd790cab5cc0665b168babe24cea67
David Wögerer <info@blickwert.at>
2024-08-29 14:00:52 +0200

    Update bw-chat.php

--- bw-chat.php
+++ bw-chat.php
@@ -9,11 +9,11 @@ Author: Ihr Name
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
-
 require_once plugin_dir_path(__FILE__) . 'includes/class-bw-chat-wordpress.php';
 require_once plugin_dir_path(__FILE__) . 'includes/class-bw-chat-frontend.php';
 require_once plugin_dir_path(__FILE__) . 'includes/class-bw-chat-plugin-page.php';
 require_once plugin_dir_path(__FILE__) . 'includes/class-bw-chat-email-check.php';
+require_once plugin_dir_path(__FILE__) . 'includes/class-bw-chat-helper.php';
 
 class BW_Chat {
 
@@ -23,6 +23,7 @@ class BW_Chat {
         new BW_Chat_Frontend();
         new BW_Chat_Plugin_Page();
         new BW_Chat_Email_Check();
+        new BW_Chat_Helper();
     }
 
     // Startet die Sitzung, wenn sie noch nicht gestartet wurde
