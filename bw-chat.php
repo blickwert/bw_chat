a403f5a078ade301549ff7c49055dd549370beb8
David Wögerer <info@blickwert.at>
2024-09-30 09:12:33 +0200

    Update bw-chat.php

--- bw-chat.php
+++ bw-chat.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: BW Chat
 Description: Ein Plugin zum Erstellen von Chat-Nachrichten und Sitzungen.
-Version: 1.0
+Version: 1.1
 Author: Ihr Name
 */
 
@@ -19,19 +19,12 @@ require_once plugin_dir_path(__FILE__) . 'includes/class-bw-chat-helper.php';
 class BW_Chat {
 
     public function __construct() {
-        add_action('init', [$this, 'start_session'], 1);
         new BW_Chat_WordPress();
         new BW_Chat_Frontend();
         new BW_Chat_Plugin_Page();
         new BW_Chat_Email_Check();
     }
 
-    // Startet die Sitzung, wenn sie noch nicht gestartet wurde
-    public function start_session() {
-        if (!session_id()) {
-            session_start();
-        }
-    }
 }
 
 new BW_Chat();
