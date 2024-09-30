cd786e057825e4163448525c4b9171f7720d4cfb
David Wögerer <info@blickwert.at>
2024-09-30 09:13:30 +0200

    Update ajax-form.js

--- js/ajax-form.js
+++ js/ajax-form.js
@@ -1,39 +1,124 @@
-jQuery(document).ready(function($) {
-    // Funktion zum Laden des Inhalts des Posts
-    function loadPostContent() {
-        $.post(ajax_form_params.ajax_url, { action: 'check_post_content', security: ajax_form_params.nonce }, function(response) {
-            if (response.success) {
-                var currentContent = $('#form-result').html();
-                if (currentContent !== response.data.message) {
-                    $('#form-result').html(response.data.message);
-                }
-            } else {
-                console.log('Es gab einen Fehler beim Abrufen des Inhalts.');
+jQuery(document).ready(function ($) {
+    // Setze die Variablen
+    var is_session_id = ajax_form_params.is_session_id;
+    var is_chat_live = ajax_form_params.is_chat_live;
+
+
+    // Cookie setzen und entfernen, wenn der Chat ge√∂ffnet oder geschlossen wird
+    $('#bw-chat-button').on('click', function () {
+        document.cookie = "bw-chat-state-is-open=" + is_session_id + "; path=/; SameSite=Lax";
+        $('#bw-chat-button').removeClass('chat-show').addClass('chat-hidden');
+        $('#bw-chat-window,#bw-chat-step-createchat').removeClass('chat-hidden').addClass('chat-show');
+    });
+
+    $('#bw-chat-close').on('click', function () {
+        document.cookie = "bw-chat-state-is-open=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        $('#bw-chat-button').removeClass('chat-hidden').addClass('chat-show');
+        $('#bw-chat-window').addClass('chat-hidden');
+    });
+
+    // √úberpr√ºfen, ob das Cookie gesetzt ist und Chat-Status basierend darauf anzeigen
+    function checkChatStatus() {
+        var chatOpenCookie = document.cookie.match(/bw-chat-state-is-open=(\w+)/);
+        if (chatOpenCookie) {
+            $('#bw-chat-button').addClass('chat-hidden');
+            $('#bw-chat-window').removeClass('chat-hidden').addClass('chat-show');
+            // Anzeigen des entsprechenden Formulars basierend auf den Variablen
+            if (!is_chat_live) {
+                $('#bw-chat-step-contactform').removeClass('chat-hidden').addClass('chat-show');
+            } else if (is_chat_live && !is_session_id) {
+                $('#bw-chat-step-createchat').removeClass('chat-hidden').addClass('chat-show');
+            } else if (is_chat_live && is_session_id) {
+                $('#bw-chat-step-welcometext').removeClass('chat-hidden').addClass('chat-show');
+                $('#chat-item').removeClass('chat-hidden').addClass('chat-show');
+                $('#bw-chat-form input').prop('disabled', false).prop('required', true);
             }
-        });
+        }
     }
 
-    // Laden Sie vorhandenen Inhalt beim Laden der Seite
-    loadPostContent();
+    checkChatStatus();
 
-    // ‹berpr¸fen Sie alle 3 Sekunden, ob sich der Inhalt des Posts ge‰ndert hat
-    setInterval(loadPostContent, 3000);
+    // Polling alle 3 Sekunden, wenn das Cookie gesetzt ist
+    if (document.cookie.indexOf("bw-chat-state-is-open") !== -1) {
+        setInterval(function () {
+            $.post(ajax_form_params.ajax_url, {
+                action: 'check_reply_emails',
+                security: ajax_form_params.nonce
+            }, function (response) {
+                if (response.success) {
+                    console.log(response.data.message);
+                    $('#chat-items').html(response.data.message);
+                } else {
+                    console.log(response.data);
+                }
+            });
+        }, 3000);
+    }
 
-    $('#ajax-form').on('submit', function(e) {
+    // Senden des Create-Chat-Formulars
+    $('#ajax-form-createchat').on('submit', function (e) {
         e.preventDefault();
-
         var formData = {
-            action: 'handle_ajax_form',
-            security: ajax_form_params.nonce,
-            name: $('#name').val()
+            action: 'handle_ajax_form_start',
+            security: ajax_form_params.nonce_start,
+            'bw-chat-userprofile-name': $('#bw-chat-userprofile-name').val(),
+            'bw-chat-userprofile-email': $('#bw-chat-userprofile-email').val(),
+            'bw-chat-userprofile-privacy': $('#bw-chat-userprofile-privacy').is(':checked')
         };
-
-        $.post(ajax_form_params.ajax_url, formData, function(response) {
+        $.post(ajax_form_params.ajax_url, formData, function (response) {
             if (response.success) {
-                $('#form-result').html(response.data.message);
+                $('#bw-chat-step-createchat').removeClass('chat-show').addClass('chat-hidden');
+                $('#bw-chat-step-welcometext').html(response.data).removeClass('chat-hidden').addClass('chat-show');
+                $('#chat-userinput-entry,#chat-userinput-submit').prop('disabled', false).prop('required', true);
+
+
             } else {
-                $('#form-result').html('Es gab einen Fehler bei der Verarbeitung.');
+                alert('Fehler: ' + response.data);
             }
         });
     });
+
+
+    // Senden des Kontaktformulars
+    $('#ajax-form-contactform').on('submit', function (e) {
+        e.preventDefault();
+        var formData = {
+            action: 'handle_ajax_form_contact',
+            security: ajax_form_params.nonce_contact,  // Verwende die Nonce f√ºr das Kontaktformular
+            'bw-chat-contact-name': $('#bw-chat-contact-name').val(),
+            'bw-chat-contact-email': $('#bw-chat-contact-email').val(),
+            'bw-chat-contact-subject': $('#bw-chat-contact-subject').val(),
+            'bw-chat-contact-message': $('#bw-chat-contact-message').val(),
+            'bw-chat-contact-privacy': $('#bw-chat-contact-privacy').is(':checked')
+        };
+        $.post(ajax_form_params.ajax_url, formData, function (response) {
+            if (response.success) {
+                alert('Kontaktformular erfolgreich gesendet.');
+            } else {
+                alert('Fehler: ' + response.data);
+            }
+        });
+    });
+    
+    
+    
+    // Senden des Benutzer-Chat-Eingabeformulars
+    $('#ajax-form-userinput').on('submit', function (e) {
+        e.preventDefault();
+        var formData = {
+            action: 'handle_ajax_form_userinput',
+            security: ajax_form_params.nonce_userinput,
+            'chat-userinput-entry': $('#chat-userinput-entry').val()
+        };
+        $.post(ajax_form_params.ajax_url, formData, function (response) {
+            if (response.success) {
+                $('#chat-items').html(response.data.message);
+                $('#chat-userinput-entry').val('');
+            } else {
+                alert('Fehler beim Senden der Nachricht.');
+            }
+        });
+    });
+    
+        console.log(is_session_id);
 });
